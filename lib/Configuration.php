823a5c6ca21a0026f07ebcf4d3ac387178862ef6
saracubillas <saracubillas@gmail.com>
2023-05-16 11:35:50 +0200

    env

--- lib/Configuration.php
+++ lib/Configuration.php
@@ -47,7 +47,7 @@ class Configuration
      *
      * @var string
      */
-    protected $host = 'https://{env}.flightaware.com/aeroapi';
+    protected $host = 'https://aereoapi.flightaware.com/aeroapi';
 
     /**
      * User agent of the HTTP request, set to "PHP-Swagger" by default
